1211820bee6432cee0113158e220b8515790d65c
imishra <imishra@paymentus.com>
2022-11-13 19:09:48 +0530

    edit and delete feature

--- todo_task.js
+++ todo_task.js
@@ -15,7 +15,7 @@ let addTask = () => {
     localStorage.setItem("todoList", JSON.stringify(todoList));
     console.log(todoList);
     listTask();
-}
+};
 
 let listTask = () => {
     tasks.innerHTML = "";
@@ -24,6 +24,10 @@ let listTask = () => {
         <span>${x.task}</span>
         <span>${x.taskDate}</span>
         <span>${x.description}</span>
+        <div id="taskBtn" style="display: flex;background-color: #e2eede;">
+        <button id="editBtn" onClick="editTask(this);toggleModal(true)"><img  src="edit.png" width="30px" height="30px"/></button>
+        <button id="deleteBtn" onClick="deleteTask(this);listTask()"><img  src="delete.png" width="30px" height="30px" /></button>
+        </div>
        </div>`) 
     });
 
@@ -34,8 +38,23 @@ let resetForm = () => {
     task.value = "";
     taskDate.value = "";
     description.value = "";
-}
+};
 
+deleteTask = (e) => {
+    e.parentElement.parentElement.remove();
+    todoList.splice(e.parentElement.parentElement.id, 1);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+};
+
+editTask = (e) => {
+    let selectedTask = e.parentElement.parentElement;
+    console.log("selected task::::", selectedTask);
+    task.value = selectedTask.children[0].innerHTML;
+    taskDate.value = selectedTask.children[1].innerHTML;
+    description.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+}
 
 (() => {
     todoList = JSON.parse(localStorage.getItem("todoList")) || []
--- toggle_modal.js
+++ toggle_modal.js
@@ -1,19 +1,12 @@
-const add = document.getElementById('addTaskBtn');
-const modal_container = document.getElementById('modal_container');
-const closeBtn = document.getElementById("closeBtn");
-const close = document.getElementById("close");
-
-add.addEventListener('click', () => {
-    
-    modal_container.classList.add('show');
-});
-
-closeBtn.addEventListener('click', () => {
-    modal_container.classList.remove('show');
-});
+toggleModal = (b) => {
+    if(b) {
+        modal_container.classList.add('show');
+    } 
+    if(!b) {
+        modal_container.classList.remove('show');
+    }
+};
 
 
-close.addEventListener('click', () => {
-    modal_container.classList.remove('show');
-});
+
 
